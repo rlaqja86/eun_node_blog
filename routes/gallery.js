f898d2fba0ce00685268dee4bfd6c02d3cbfbf59
k1b219 <kqw1209@gmail.com>
2017-07-30 16:39:34 +0900

    nodejs setting

--- routes/gallery.js
+++ routes/gallery.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('main', { title: 'this is gallery' });
+    var http = require('http');
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://localhost:27017/local";
+
+    MongoClient.connect(url, function(err, db) { 
+        if (err) throw err; 
+        var query = { categoryName: "메인메뉴" }
+        db.collection("category").find(query).toArray(function(err, result) {
+            if (err) throw err;   
+            console.log(result);
+            res.render('gallery', { title: result });   
+            db.close(); 
+        });
+    });
 });
 
-module.exports = router;
+        module.exports = router;
