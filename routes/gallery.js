fcf733c4e90c8296640d1380442397dedadb5d49
daewooChoi <cdw6845@gmail.com>
2017-07-30 16:41:29 +0900

    css 수정

--- routes/gallery.js
+++ routes/gallery.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('main', { title: 'this is gallery' });
+    res.render('gallery', { title: 'this is gallery' });
 });
 
 module.exports = router;
