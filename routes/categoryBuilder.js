75303f6da71b536f484ab1586b413d38a51ec114
k1b219 <kqw1209@gmail.com>
2017-08-13 18:48:23 +0900

    refactoring

--- routes/categoryBuilder.js
+++ routes/categoryBuilder.js
@@ -3,41 +3,43 @@ var router = express.Router();
 var queryString = require('querystring')
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/local";
+var http = require('http');
 
-/* GET home page. */
+const CATEGORY_COLLECTION_NAME = "category"
+
+/* save category. */
 router.post('/save', function(req, res, next) {
-    var http = require('http');
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var categoryName = req.param('name');
-        db.collection("category").save(createCategory(categoryName));
-        res.redirect('/categoryBuilder');
-    });
-});
-
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    var http = require('http');
-    var MongoClient = require('mongodb').MongoClient;
-    var url = "mongodb://localhost:27017/local";
-    MongoClient.connect(url, function(err, db) { 
-        if (err) throw err; 
-        var query = { categoryName: "메인메뉴" }
-        db.collection("category").find().toArray(function(err, result) {
-            res.status(200).render('admin', { categories: result });  
-            db.close(); 
+    try {
+        MongoClient.connect(url, function(err, db) {
+        var categoryName = req.param('name');        
+        db.collection(CATEGORY_COLLECTION_NAME).save(createCategory(categoryName));
+         res.redirect('/categoryBuilder');
         });
-    });
+        db.close();
+    } catch(excpetion) {
+        console.log(exception);
+    }
+ 
 });
 
-//임시적으로 카테고리 생성해주는것
-//실제로는 web에서 등록된 값을 해당 함수에 넣어주면 됨!
+/* get admin main page */
+router.get('/', function(req, res, next) {
+    try {
+        MongoClient.connect(url, function(err, db) {
+        db.collection("category").find().toArray(function(err, result) {
+        res.status(200).render('admin', { categories: result });  
+        db.close(); 
+    });    
+});
+    } catch (exception) {
+        console.log(exception);
+    }
+});
+
 function createCategory(categoryName) {
     var category = require('../bin/domain/Category');
     category.name = categoryName;
     return category;
 }
-
 
 module.exports = router;
