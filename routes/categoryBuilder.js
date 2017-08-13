ebaa3dde8bfe3385d8922b75ce22e394bcf2b075
k1b219 <kqw1209@gmail.com>
2017-08-13 18:14:32 +0900

    push push baby
    Signed-off-by: k1b219 <kqw1209@gmail.com>

--- routes/categoryBuilder.js
+++ routes/categoryBuilder.js
@@ -1,47 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var queryString = require('querystring')
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/local";
 
 /* GET home page. */
-router.get('/save', function (req, res, next) {
+router.post('/save', function(req, res, next) {
     var http = require('http');
-    var MongoClient = require('mongodb').MongoClient;
-    var url = "mongodb://localhost:27017/local";
-
-    MongoClient.connect(url, function (err, db) {
+    MongoClient.connect(url, function(err, db) {
         if (err) throw err;
-        var query = { categoryName: "메인메뉴" }
-        db.collection("category").save(createCategory("n","testCategory","3",""));
+        var categoryName = req.param('name');
+        db.collection("category").save(createCategory(categoryName));
     });
-   // res.render('gallery', { title: 'this is gallery' });
 });
 
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
     var http = require('http');
     var MongoClient = require('mongodb').MongoClient;
     var url = "mongodb://localhost:27017/local";
-      MongoClient.connect(url, function(err, db) { 
+    MongoClient.connect(url, function(err, db) { 
         if (err) throw err; 
         var query = { categoryName: "메인메뉴" }
         db.collection("category").find().toArray(function(err, result) {
-        console.log(JSON.stringify(result))
-        console.log(result)
-         res.status(200).render('admin', { categories: JSON.stringify(result)});  
-         db.close(); 
-        }); 
+            res.status(200).render('admin', { categories: result });  
+            db.close(); 
+        });
     });
 });
 
 //임시적으로 카테고리 생성해주는것
 //실제로는 web에서 등록된 값을 해당 함수에 넣어주면 됨!
-function createCategory(categoryId, categoryName, depth, childcategories) {
+function createCategory(categoryName) {
     var category = require('../bin/domain/Category');
-    category.categoryId = categoryId;
     category.name = categoryName;
-    category.depth = depth;
-    category.childCategories = childcategories;
-    console.log(category);
     return category;
 }
 
