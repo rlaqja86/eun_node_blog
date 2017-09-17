f25f4b7ee743b15a01ab493799b4df4eca8cf2d4
daewooChoi <cdw6845@gmail.com>
2017-09-17 23:33:31 +0900

    promise, request module 추가, index 코드 중복 개선, DB전체삭제 추가

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
+var request = require('request');
 var path = require('path');
-var _promise = require('promise');
 var MongoClient = require('mongodb').MongoClient;
-var DB_CONSTANT = require(path.join(__dirname, '../bin/domain/DB_CONSTANT'));
 var router = express.Router();
 
+var DB_CONSTANT = require(path.join(__dirname, '../bin/domain/DB_CONSTANT'));
 var DB_URL = DB_CONSTANT.url;
 var PROJECT_COLLECTION_NAME = DB_CONSTANT.mainCollection;
 var PROJECT_PART = 2; // 한페이지에 보여줄 프로젝트수, 관리자가 정한다
@@ -25,28 +25,37 @@ var EMPTY_PROJECT = [{
 }];
 
 // 데이터베이스 조회 후 최근프로젝트순으로 정렬된 배열(result)을 projects변수에 담아 index.jade로 넘긴다
-// images는 원래 배열이지만 index페이지를 구성할 때는 isMain이 true인 객체만 가져온다.
+// images는 원래 배열이지만 index페이지를 구성할 때는 isMain이 true인 객체 하나만 가져온다.
 router.get('/', function(req, res, next) {
     try {
         MongoClient.connect(DB_URL, function(err, db) {
             db.collection(PROJECT_COLLECTION_NAME).count(function(err, count) {
                 totalProjectNum = count;
                 totalPageNum = Math.ceil(totalProjectNum / PROJECT_PART);
-
-                db.collection(PROJECT_COLLECTION_NAME).aggregate([
-                    { $unwind: "$images" },
-                    { $match: { "images.isMain": "true" } },
-                    { $sort: { "date": -1, "name": 1 } }
-                ]).limit(PROJECT_PART).toArray(function(err, result) {
-                    if (totalProjectNum === 0) {
-                        result = EMPTY_PROJECT;
+                db.close();
+                var result = EMPTY_PROJECT;
+                request('http://localhost:3000/page/1', function(error, response, data) {
+                    if (totalProjectNum > 0 && !error && response.statusCode === 200) {
+                        result = JSON.parse(data);
+                    } else {
+                        console.log('error:', error);
+                        console.log('statusCode:', response && response.statusCode);
                     }
                     res.render('index', { title: "hellomate", projects: result, totalPageNum: totalPageNum });
-                    db.close();
                 });
-
             });
+        });
+    } catch (ex) {
+        console.log(ex);
+    }
+});
 
+router.get('/resetDB', function(req, res, next) {
+    try {
+        MongoClient.connect(DB_URL, function(err, db) {
+            db.collection(PROJECT_COLLECTION_NAME).drop();
+            db.close(); 
+            res.redirect('/');
         });
     } catch (ex) {
         console.log(ex);
@@ -59,7 +68,6 @@ router.get('/page/:page', function(req, res, next) {
     if (page <= totalPageNum && page > 0) {
         try {
             MongoClient.connect(DB_URL, function(err, db) {
-
                 db.collection(PROJECT_COLLECTION_NAME).aggregate([
                     { $unwind: "$images" },
                     { $match: { "images.isMain": "true" } },
@@ -73,30 +81,7 @@ router.get('/page/:page', function(req, res, next) {
             console.log(ex);
         }
     } else {
-        // res.redirect('/page/1');
         res.send(404);
     }
 });
-
-function loadProjects(page) {
-    return new Promise(function(resolved, rejected) {
-        try {
-            MongoClient.connect(DB_URL, function(err, db) {
-
-                db.collection(PROJECT_COLLECTION_NAME).aggregate([
-                    { $unwind: "$images" },
-                    { $match: { "images.isMain": "true" } },
-                    { $sort: { "date": -1, "name": 1 } }
-                ]).skip((page - 1) * PROJECT_PART).limit(PROJECT_PART).toArray(function(err, result) {
-                    //res.json(result);
-                    console.log(result);
-                    db.close(); 
-                    return result;
-                });
-            });
-        } catch (ex) {
-            console.log(ex);
-        }
-    });
-}
 module.exports = router;
