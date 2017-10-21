3e61086399c74e498213b861640c3a001c84d542
daewooChoi <cdw6845@gmail.com>
2017-10-21 21:17:45 +0900

    스크롤 로드 템플릿 생성

---
+++ js/template.js
@@ -0,0 +1,6 @@
+<div id="item" class="grid-item">
+    <img src="{{images.image}}" alt="{{images.name}}" />
+    <a href="/project/{{name}}">
+        <div class="caption" data-image="{{images.image}}" data-name="{{name}}" data-description="{{description}}" data-date="{{date}}">{{name}}</div>
+    </a>
+</div>
